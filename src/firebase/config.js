6ed4de3920fa744a258d1d46abf7c5333a614494
harsh jaiswal <tech.harsh2003@gmail.com>
2023-08-05 10:51:15 +0530

    firebase added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,16 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  // apiKey: "AIzaSyCggZCcBun0cwNfOWGC2K8pZcgIRWMfqwY",
-  // authDomain: "olx-sijeesh.firebaseapp.com",
-  // projectId: "olx-sijeesh",
-  // storageBucket: "olx-sijeesh.appspot.com",
-  // messagingSenderId: "767411886432",
-  // appId: "1:767411886432:web:2ef6862afc88f2c423a605",
-  // measurementId: "G-4ELNR9DJHL"
   apiKey: "AIzaSyA9J-roVQQHJqvXVzunk6EIJwv1pVipBPk",
   authDomain: "epics-5659a.firebaseapp.com",
   projectId: "epics-5659a",
@@ -21,9 +12,4 @@ const firebaseConfig = {
   measurementId: "G-XK9GWWX5S5",
 };
 
-export const Firebase = firebase.initializeApp(firebaseConfig); //named export
-// const app = initializeApp(firebaseConfig);
-// export const Firebase = getFirestore(app);
-// firebase.initializeApp(firebaseConfig);
-// var Firebase = firebase.firestore();
-// export {Firebase};
+export const Firebase = firebase.initializeApp(firebaseConfig);
