0950bdd8cea99ea7e516f15293c0482c674cb9d7
harsh jaiswal <tech.harsh2003@gmail.com>
2023-12-01 21:03:14 +0530

    hosting

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const dotenv = require("dotenv");
+// dotenv.config();
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -15,6 +16,4 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-export const Firebase = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+export const Firebase = firebase.initializeApp(firebaseConfig);
